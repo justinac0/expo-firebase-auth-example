ee3bfb0bf427fc4fa23c649fe59a3d0eb8df84b3
justinac0 <justinac.1999@gmail.com>
2022-07-10 00:54:38 +1000

    Send message to firestore.

--- source/firebase/config.ts
+++ source/firebase/config.ts
@@ -1,6 +1,11 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
 import firebaseConfig from "./firebase_config.json"
 
 const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+// const auth = getAuth();
 
-export { firebaseApp };
+export { firebaseApp, db };
--- source/screens/ManageScreen.tsx
+++ source/screens/ManageScreen.tsx
@@ -1,5 +1,8 @@
 import { View, Text } from "react-native";
 
+import { setDoc, doc, serverTimestamp } from "firebase/firestore";
+import { db } from "../firebase/config";
+
 import { Colors, globalStyles } from "../styles/GlobalStyle";
 import Button from "../components/Button";
 import Break from "../components/Break";
@@ -8,19 +11,42 @@ import { useAuth } from "../context/AuthProvider";
 
 const ManageScreen = ({ navigation }) => {
   const auth = useAuth();
+
+  const addSomeDataPress = async () => {
+    const timestamp = serverTimestamp();
+    const data = {
+      uid: auth.currentUser.uid,
+      message: "hello application user...."
+    }
+
+    const status = await setDoc(doc(db, "messages", data.uid), data);
+    console.log(status);
+  };
+
   return (
     <View style={globalStyles.container}>
-        <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit similique repudiandae esse ipsa non, ducimus id tempora exercitationem eius aliquam quam dolor, nulla hic debitis adipisci doloremque magni inventore? Porro, reprehenderit accusantium sint laudantium fugiat beatae optio alias accusamus deserunt adipisci voluptate, maiores quas praesentium architecto assumenda sunt quibusdam et?</Text>
-        <Break />
-        {auth.isOffline ?
+      <Text>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit similique
+        repudiandae esse ipsa non, ducimus id tempora exercitationem eius
+        aliquam quam dolor, nulla hic debitis adipisci doloremque magni
+        inventore? Porro, reprehenderit accusantium sint laudantium fugiat
+        beatae optio alias accusamus deserunt adipisci voluptate, maiores quas
+        praesentium architecto assumenda sunt quibusdam et?
+      </Text>
+      <Break />
+      {auth.isOffline ? (
         <></>
-        :
-        <Button title="Only for Authentic Users" onPress={()=>{
-          alert("You are Authentic!");
-        }}
-        bgColor={Colors.RED} />
-        }
-        </View>
+      ) : (
+        <Button
+          title="Only for Authentic Users"
+          onPress={() => {
+            alert("You are Authentic!");
+            addSomeDataPress();
+          }}
+          bgColor={Colors.RED}
+        />
+      )}
+    </View>
   );
 };
 
